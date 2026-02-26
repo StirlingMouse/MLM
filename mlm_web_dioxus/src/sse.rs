7313ac4026925f87622518a0644509225033776d
Stirling Mouse <181794392+StirlingMouse@users.noreply.github.com>
2026-02-26 19:44:00 +0100

    live selected and errors pages

--- mlm_web_dioxus/src/sse.rs
+++ mlm_web_dioxus/src/sse.rs
@@ -2,19 +2,41 @@ use dioxus::prelude::*;
 
 pub static STATS_UPDATE_TRIGGER: GlobalSignal<u32> = Signal::global(|| 0);
 pub static EVENTS_UPDATE_TRIGGER: GlobalSignal<u32> = Signal::global(|| 0);
+pub static SELECTED_UPDATE_TRIGGER: GlobalSignal<u32> = Signal::global(|| 0);
+pub static ERRORS_UPDATE_TRIGGER: GlobalSignal<u32> = Signal::global(|| 0);
+pub static QBIT_PROGRESS: GlobalSignal<Vec<(u64, u32)>> = Signal::global(Vec::new);
 
 pub fn trigger_stats_update() {
     #[cfg(not(feature = "server"))]
     {
-        let mut val = STATS_UPDATE_TRIGGER.write();
-        *val += 1;
+        *STATS_UPDATE_TRIGGER.write() += 1;
     }
 }
 
 pub fn trigger_events_update() {
     #[cfg(not(feature = "server"))]
     {
-        let mut val = EVENTS_UPDATE_TRIGGER.write();
-        *val += 1;
+        *EVENTS_UPDATE_TRIGGER.write() += 1;
+    }
+}
+
+pub fn trigger_selected_update() {
+    #[cfg(not(feature = "server"))]
+    {
+        *SELECTED_UPDATE_TRIGGER.write() += 1;
+    }
+}
+
+pub fn trigger_errors_update() {
+    #[cfg(not(feature = "server"))]
+    {
+        *ERRORS_UPDATE_TRIGGER.write() += 1;
+    }
+}
+
+pub fn update_qbit_progress(progress: Vec<(u64, u32)>) {
+    #[cfg(not(feature = "server"))]
+    {
+        *QBIT_PROGRESS.write() = progress;
     }
 }
